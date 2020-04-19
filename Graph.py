a77350ae0e12d3a8249abde56305460f0c585cb0
chritophersvillamayor <37824002+chritophersvillamayor@users.noreply.github.com>
2020-04-19 12:09:14 -0500

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -1,53 +1,56 @@
 import random
 
+
 class Node:
     def __init__(self, val):
         self.val = val
         self.adj = list()
-        self.adjLetters = list()
 
 
 class Graph:
     def __init__(self):
         self.vertices = list()
-        self.letters = list()
 
     def addNode(self, val: str):
         n = Node(val)
         self.vertices.append(n)
-        self.letters.append(val)
 
     def addUndirectedEdge(self, first: Node, second: Node):
         if first in self.vertices:
             first.adj.append(second)
-            first.adjLetters.append(second.val)
         else:
+            print("ERROR: node " + first.val + " is not in the graph")
             return -1
 
         if second in self.vertices:
             second.adj.append(first)
-            second.adjLetters.append(first.val)
         else:
+            print("ERROR: node " + second.val + " is not in the graph")
             return -1
 
     def removeUndirectedEdge(self, first: Node, second: Node):
+        if first not in self.vertices or second not in self.vertices:
+            print("ERROR: given nodes are not in the graph")
+            return -1
+
         if second in first.adj:
             first.adj.remove(second)
-        else:
-            return -1
 
         if first in second.adj:
             second.adj.remove(first)
-        else:
-            return -1
 
     def getNode(self, val):
-        return self.vertices[self.letters.index(val)]
+        for node in self.vertices:
+            if node.val == val:
+                return node
 
     def getAllNodes(self):
-        output = {}
-        for n in self.vertices:
-            output[n.val] = n.adjLetters
+        output = dict()
+
+        for node in self.vertices:
+            output[node.val] = list()
+            for neighbor in node.adj:
+                output[node.val].append(neighbor.val)
 
         return output
 
@@ -72,9 +75,10 @@ def createLinkedList(n: int):
     for i in range(1, n):
         nextNode = Node(str(i))
         g.vertices.append(nextNode)
-        g.letters.append(nextNode.val)
-
         g.vertices[i-1].adj.append(nextNode)
-        g.vertices[i-1].adjLetters.append(nextNode.val)
 
     return g
+
+
+g = createLinkedList(5)
+print(g.getAllNodes())
